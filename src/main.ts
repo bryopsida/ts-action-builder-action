aa1cc039f6e34401681e8982450ef300af060b67
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-12-17 09:02:50 -0600

    more logging

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import {spawn} from 'child_process'
 // TODO refactor to async/await and/or breakup calls, prove it works then clean it up.
 async function run(): Promise<void> {
   try {
+    core.info('Running build')
     const buildProc = spawn('npm', ['run', 'build'])
     buildProc.stdout.pipe(process.stdout)
     buildProc.stderr.pipe(process.stderr)
     buildProc.on('close', code => {
       if (code !== 0) return core.setFailed('Build failed')
-
+      core.info('Running package')
       const packageProc = spawn('npm', ['run', 'package'])
       packageProc.stdout.pipe(process.stdout)
       packageProc.stderr.pipe(process.stderr)
