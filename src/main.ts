a749d6e7ce7151b7824a1f2f90131e309604dad1
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-12-17 09:23:08 -0600

    Initial implementation (#10)
    * Distibution build after dependency update
    
    * condense to one ci workflow
    
    * Distibution build after dependency update
    
    * more logging
    
    * Distibution build after dependency update
    
    * Distibution build after dependency update
    
    * check if this works
    
    * Distibution build after dependency update

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,56 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {simpleGit} from 'simple-git'
+import {spawn} from 'child_process'
 
+// TODO refactor to async/await and/or breakup calls, prove it works then clean it up.
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.info('Running install')
+    const installProc = spawn('npm', ['install', '--ignore-scripts'])
+    installProc.stdout.pipe(process.stdout)
+    installProc.stderr.pipe(process.stderr)
+    installProc.on('close', installCode => {
+      if (installCode !== 0) return core.setFailed('Install failed')
+      core.info('Running build')
+      const buildProc = spawn('npm', ['run', 'build'])
+      buildProc.stdout.pipe(process.stdout)
+      buildProc.stderr.pipe(process.stderr)
+      buildProc.on('close', code => {
+        if (code !== 0) return core.setFailed('Build failed')
+        core.info('Running package')
+        const packageProc = spawn('npm', ['run', 'package'])
+        packageProc.stdout.pipe(process.stdout)
+        packageProc.stderr.pipe(process.stderr)
+        packageProc.on('close', packageCode => {
+          if (packageCode !== 0) return core.setFailed('Package failed')
+          const git = simpleGit()
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+          git.status({}, (err, result) => {
+            if (err) return core.setFailed(err)
+            if (result.isClean()) return
+            core.info('Detected a build is required')
 
-    core.setOutput('time', new Date().toTimeString())
+            /// stage files
+            git.add('.', addErr => {
+              if (addErr) return core.setFailed(addErr)
+
+              git.commit(
+                'Distibution build after dependency update',
+                commitErr => {
+                  if (commitErr) return core.setFailed(commitErr)
+
+                  // push back to remote
+                  git.push(pushErr => {
+                    if (pushErr) return core.setFailed(pushErr)
+                    core.info('Finished updating build')
+                  })
+                }
+              )
+            })
+          })
+        })
+      })
+    })
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
