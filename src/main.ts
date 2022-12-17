9678c6dccdb6dd3e9633d88ba3188c6010b36ffb
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-12-17 08:45:34 -0600

    Distibution build after dependency update

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,44 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {simpleGit} from 'simple-git'
+import {spawn} from 'child_process'
 
+// TODO refactor to async/await and/or breakup calls, prove it works then clean it up.
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const buildProc = spawn('npm', ['run', 'build'])
+    buildProc.on('close', code => {
+      if (code !== 0) return core.setFailed('Build failed')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+      const packageProc = spawn('npm', ['run', 'package'])
+      packageProc.on('close', packageCode => {
+        if (packageCode !== 0) return core.setFailed('Package failed')
+        const git = simpleGit()
 
-    core.setOutput('time', new Date().toTimeString())
+        git.status({}, (err, result) => {
+          if (err) return core.setFailed(err)
+          if (result.isClean()) return
+          core.info('Detected a build is required')
+
+          /// stage files
+          git.add('.', addErr => {
+            if (addErr) return core.setFailed(addErr)
+
+            git.commit(
+              'Distibution build after dependency update',
+              commitErr => {
+                if (commitErr) return core.setFailed(commitErr)
+
+                // push back to remote
+                git.push(pushErr => {
+                  if (pushErr) return core.setFailed(pushErr)
+                  core.info('Finished updating build')
+                })
+              }
+            )
+          })
+        })
+      })
+    })
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
